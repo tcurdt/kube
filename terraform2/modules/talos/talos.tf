4f0cd87d746bb2e9953f8426d1f4d2deb242f299
Torsten Curdt <tcurdt@vafer.org>
2025-02-06 00:23:28 +0100

    patches

--- terraform2/modules/talos/talos.tf
+++ terraform2/modules/talos/talos.tf
@@ -3,66 +3,90 @@ resource "talos_machine_secrets" "this" {
 }
 
 data "talos_machine_configuration" "controlplane" {
-  // talos_version = "v1.9.3"
-  cluster_name         = var.cluster_name
+  # talos_version      = "v1.9.3"
+  # kubernetes_version = var.kubernetes_version
+  cluster_name     = var.cluster_name
   cluster_endpoint = "https://${hcloud_server.control_plane[0].ipv4_address}:6443"
-  // kubernetes_version = var.kubernetes_version
   machine_type     = "controlplane"
   machine_secrets  = talos_machine_secrets.this.machine_secrets
   config_patches = concat([
+
+    yamlencode({
+      cluster = {
+        allowSchedulingOnControlPlanes = true
+      }
+    }),
+
+    yamlencode({
+      machine = {
+        kubelet = {
+          extraMounts = [
+            {
+              destination = "/var/local-path-provisioner"
+              type        = "bind"
+              source      = "/var/local-path-provisioner"
+              options     = ["bind", "rshared", "rw"]
+            }
+          ]
+        }
+      }
+    }),
+
   ])
-  docs = false
+  docs     = false
   examples = false
 }
 
 data "talos_machine_configuration" "worker" {
-  // talos_version = "v1.9.3"
-  cluster_name         = var.cluster_name
+  # talos_version      = "v1.9.3"
+  # kubernetes_version = var.kubernetes_version
+  cluster_name     = var.cluster_name
   cluster_endpoint = "https://${hcloud_server.control_plane[0].ipv4_address}:6443"
-  // kubernetes_version = var.kubernetes_version
   machine_type     = "worker"
   machine_secrets  = talos_machine_secrets.this.machine_secrets
   config_patches = concat([
 
+    yamlencode({
+      cluster = {
+        allowSchedulingOnControlPlanes = true
+      }
+    }),
+
+    yamlencode({
+      machine = {
+        kubelet = {
+          extraMounts = [
+            {
+              destination = "/var/local-path-provisioner"
+              type        = "bind"
+              source      = "/var/local-path-provisioner"
+              options     = ["bind", "rshared", "rw"]
+            }
+          ]
+        }
+      }
+    }),
+
     # yamlencode({
     #   machine = {
-    #     network = {
-    #       interfaces = [
-    #         {
-    #           interface = "eth1"
-    #           addresses = ["10.0.1.${count.index + 10}/16"]
-    #         }
-    #       ]
+    #     env = {
+    #       HCLOUD_TOKEN = var.hcloud_token
     #     }
     #   }
-    # }),
-    # yamlencode({
     #   cluster = {
     #     externalCloudProvider = {
     #       enabled = true
     #       manifests = [
-    #         # Hetzner Cloud Controller Manager
     #         {
     #           name = "hcloud-ccm"
     #           url  = "https://github.com/hetznercloud/hcloud-cloud-controller-manager/releases/latest/download/ccm-networks.yaml"
     #         },
-    #         # Hetzner CSI Driver
     #         {
     #           name = "hcloud-csi"
     #           url  = "https://raw.githubusercontent.com/hetznercloud/csi-driver/v2.5.1/deploy/kubernetes/hcloud-csi.yml"
     #         }
     #       ]
     #     }
-    #   },
-    #   machine = {
-    #     env = {
-    #       HCLOUD_TOKEN = var.hcloud_token
-    #     }
-    #   }
-    # }),
-    # yamlencode({
-    #   cluster = {
-    #     allowSchedulingOnControlPlanes = false
     #     storage = {
     #       config = {
     #         storageClasses = [{
@@ -82,7 +106,7 @@ data "talos_machine_configuration" "worker" {
     # })
 
   ])
-  docs = false
+  docs     = false
   examples = false
 }
 
