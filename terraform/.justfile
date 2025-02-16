11d31ec7cc505482f9981ef589ee2c6240e53ca8
Torsten Curdt <tcurdt@vafer.org>
2025-02-16 23:25:57 +0100

    refactor

--- terraform/.justfile
+++ terraform/.justfile
@@ -14,26 +14,63 @@ talos-download:
 talos-upload:
     # --image-url "https://github.com/siderolabs/talos/releases/download/v1.9.3/metal-amd64.raw.zst"
     hcloud-upload-image -v upload \
-        --server-type cpx11 \
+        --server-type cpx21 \
         --image-path metal-amd64.raw.xz \
         --compression xz \
-        --labels "talos=v1.9.3"
+        --labels "talos=v1.9.3,type=cpx21"
 images:
-    hcloud image list
+    hcloud image list --output columns=id,type,labels
 
 init:
     tofu init
 
 plan:
-    touch .kubeconfig
     SOPS_AGE_KEY_FILE=.sops.age \
     tofu plan -var-file=.env.tfvars
 
 apply:
-    touch .kubeconfig
     SOPS_AGE_KEY_FILE=.sops.age \
     tofu apply -var-file=.env.tfvars
 
 destroy:
     SOPS_AGE_KEY_FILE=.sops.age \
     tofu destroy -var-file=.env.tfvars
+
+top:
+    kubectl --kubeconfig .configs/a/kubeconfig top pods -A
+
+k9s:
+    k9s --kubeconfig .configs/a/kubeconfig
+
+nodes:
+    kubectl --kubeconfig .configs/a/kubeconfig get nodes -o wide
+
+all:
+    kubectl --kubeconfig .configs/a/kubeconfig get all -A
+
+crds:
+    kubectl --kubeconfig .configs/a/kubeconfig get crds -A
+
+ks:
+    kubectl --kubeconfig .configs/a/kubeconfig get ks -A
+
+secrets:
+    kubectl --kubeconfig .configs/a/kubeconfig get secrets -A
+
+postgres:
+    kubectl --kubeconfig .configs/a/kubeconfig cnpg status edkimo -n demo
+
+# kubectl --kubeconfig .configs/a/kubeconfig describe node
+# kubectl --kubeconfig .configs/a/kubeconfig get pods --all-namespaces | grep Pending | awk '{print $2 " -n " $1}' | xargs -L1 kubectl describe pod
+
+talos:
+    talosctl --talosconfig .configs/a/talosconfig health --nodes 10.0.1.2
+
+flux:
+    kubectl --kubeconfig .configs/a/kubeconfig describe pods -n flux-system
+
+watch_repo:
+    kubectl --kubeconfig .configs/a/kubeconfig get gitrepositories -A -w
+
+watch_apply:
+    kubectl --kubeconfig .configs/a/kubeconfig get kustomizations -A -w
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,112 +12,62 @@ variable "aws_secret_key" {
   sensitive = true
 }
 
-variable "region" {
+variable "aws_region" {
   type    = string
   default = "eu-central-1"
 }
 
 
-# common
 
-variable "prefix" {
-  type        = string
-  description = "cluster name and prefix for the resources"
-  default     = ""
-}
+# variable "talos_image" {
+#   description = "ID or name of the Talos image in Hetzner Cloud"
+#   type        = string
+# }
 
-variable "email" {
-  type = string
-}
-
-
-# hetzner machines
-
-variable "image" {
-  type    = string
-  default = "debian-12"
-  # hcloud image list
-}
-
-variable "server_type" {
-  type    = string
-  default = "cpx11"
-  # hcloud server-type list
-}
-
-variable "location" {
-  type    = string
-  default = "fsn1"
-  # hcloud location list
-}
-
-
-# kubernetes
-
-variable "control_plane_nodes" {
-  type        = list(string)
-  description = "names of control plane nodes"
-  default     = []
-}
-
-variable "worker_nodes" {
-  type        = list(string)
-  description = "names of worker-only nodes"
-  default     = []
-}
-
+# variable "ssh_key_id" {
+#   description = "ID of the SSH key to use for the nodes"
+#   type        = string
+# }
 
 # flux
 
-variable "flux_repository" {
-  type = string
-  # full url to the git repository
-}
+# variable "flux_repository" {
+#   type = string
+#   # full url to the git repository
+# }
 
-variable "flux_branch" {
-  type    = string
-  default = "main"
-}
+# variable "flux_branch" {
+#   type    = string
+#   default = "main"
+# }
 
-variable "flux_path" {
-  type    = string
-  default = "./flux/clusters/production"
-}
-
-locals {
-  is_single_node = length(var.control_plane_nodes) == 1 && length(var.worker_nodes) == 0
-  all_nodes      = concat(var.control_plane_nodes, var.worker_nodes)
-}
-
+# variable "flux_path" {
+#   type    = string
+#   default = "./flux/clusters/production"
+# }
 
 # s3
 
-variable "s3_storage_buckets" {
-  type        = list(string)
-  description = "backup buckets names"
-}
+# variable "s3_storage_buckets" {
+#   type        = list(string)
+#   description = "backup buckets names"
+# }
 
-variable "s3_storage_keep_days" {
-  type    = number
-  default = 1
-}
+# variable "s3_storage_keep_days" {
+#   type    = number
+#   default = 1
+# }
 
 
-variable "s3_backup_buckets" {
-  type        = list(string)
-  description = "backup buckets names"
-}
+# variable "s3_backup_buckets" {
+#   type        = list(string)
+#   description = "backup buckets names"
+# }
 
-variable "s3_backup_keep_days" {
-  type    = number
-  default = 1
-}
+# variable "s3_backup_keep_days" {
+#   type    = number
+#   default = 1
+# }
 
 
 # polly
-
-variable "create_aws_resources" {
-  type        = bool
-  description = "Whether to create AWS resources"
-  default     = false
-}
